3d0768823cf54deecda0431f43f3f41beb69a3e6
MurdoNicolai <murdo.nicolai.pro@gmail.com>
2023-07-25 12:32:44 +0200

    q7 but only if dimentions match

--- math/linear_algebra/7-gettin_cozy.py
+++ math/linear_algebra/7-gettin_cozy.py
@@ -5,10 +5,11 @@
 
 def cat_matrices2D(mat1, mat2, axis=0):
     """ concatenates two matrices along a axis(0 y axis, 1 x axis)"""
+
+    if len(mat1) != len(mat2) or len(mat1[0]) != len(mat2[0]):
+        return None
     if axis == 0:
         return mat1 + mat2
-    if len(mat1) != len(mat2):
-        return None
     newmat = []
     for rownb in range(len(mat1)):
         newmat.append(mat1[rownb] + mat2[rownb])
