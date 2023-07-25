7b8394636ada5c795882965b8831d5f3892f46c5
MurdoNicolai <murdo.nicolai.pro@gmail.com>
2023-07-25 16:59:11 +0200

    q11 bis

--- math/linear_algebra/11-the_western_exchange.py
+++ math/linear_algebra/11-the_western_exchange.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python3
-import numpy as np
 """contains shape function"""
 
 
 def np_transpose(matrix):
     """returns shape of numpy matrix"""
-    return np.transpose(matrix)
+    return matrix.transpose()
