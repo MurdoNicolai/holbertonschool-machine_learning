f05c470315dfce70d138230310925e26621496fc
MurdoNicolai <murdo.nicolai.pro@gmail.com>
2023-07-25 11:20:34 +0200

    add q3 without imports

--- math/linear_algebra/3-flip_me_over.py
+++ math/linear_algebra/3-flip_me_over.py
@@ -1,14 +1,24 @@
 #!/usr/bin/env python3
-import numpy
 
 
 def matrix_transpose(matrix):
     # return matrix transposed
     rowLength = len(matrix[0])
-    trans_mat = numpy.ones((rowLength, len(matrix)))
-    nbrow = 0
+    trans_mat = []
+    for rows in range(rowLength):
+        trans_mat.append([])
+
     for row in matrix:
         for position in range(rowLength):
-            trans_mat[position][nbrow] = row[position]
-        nbrow += 1
-    return trans_mat.astype(int)
+            trans_mat[position].append(row[position])
+
+    return trans_mat
+
+
+mat1 = [[1, 2], [3, 4]]
+print(mat1)
+print(matrix_transpose(mat1))
+mat2 = [[1, 2, 3, 4, 5], [6, 7, 8, 9, 10], [11, 12, 13, 14, 15],
+        [16, 17, 18, 19, 20], [21, 22, 23, 24, 25], [26, 27, 28, 29, 30]]
+print(mat2)
+print(matrix_transpose(mat2))
