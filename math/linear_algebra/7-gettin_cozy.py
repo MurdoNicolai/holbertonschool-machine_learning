4ce377a03b360135cea504c2bde3c40da4803e02
MurdoNicolai <murdo.nicolai.pro@gmail.com>
2023-07-25 12:31:21 +0200

    add q7

---
+++ math/linear_algebra/7-gettin_cozy.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+"""contains concatenation function"""
+
+
+def cat_matrices2D(mat1, mat2, axis=0):
+    """ concatenates two matrices along a axis(0 y axis, 1 x axis)"""
+    if axis == 0:
+        return mat1 + mat2
+    if len(mat1) != len(mat2):
+        return None
+    newmat = []
+    for rownb in range(len(mat1)):
+        newmat.append(mat1[rownb] + mat2[rownb])
+    return newmat
