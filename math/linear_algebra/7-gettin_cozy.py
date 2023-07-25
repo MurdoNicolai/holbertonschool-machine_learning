b6d15c15c144cea262d31361a92b29f1b889a85b
MurdoNicolai <murdo.nicolai.pro@gmail.com>
2023-07-25 12:36:59 +0200

    q7 change position again

--- math/linear_algebra/7-gettin_cozy.py
+++ math/linear_algebra/7-gettin_cozy.py
@@ -6,9 +6,9 @@
 def cat_matrices2D(mat1, mat2, axis=0):
     """ concatenates two matrices along a axis(0 y axis, 1 x axis)"""
 
-    if len(mat1[0]) != len(mat2[0]):
-        return None
     if axis == 0:
+        if len(mat1[0]) != len(mat2[0]):
+            return None
         return mat1 + mat2
     if len(mat1) != len(mat2):
         return None
