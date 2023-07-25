0f3899500ca4632d42161b9118629927c2cb6feb
MurdoNicolai <murdo.nicolai.pro@gmail.com>
2023-07-25 11:32:03 +0200

    q3 add comments

--- math/linear_algebra/3-flip_me_over.py
+++ math/linear_algebra/3-flip_me_over.py
@@ -5,10 +5,13 @@ def matrix_transpose(matrix):
     # return matrix transposed
     rowLength = len(matrix[0])
     trans_mat = []
+
     for rows in range(rowLength):
+        # create new matrix with correct nb rows
         trans_mat.append([])
 
     for row in matrix:
+        # go through each valu and put it in the transposed matrix
         for position in range(rowLength):
             trans_mat[position].append(row[position])
 
