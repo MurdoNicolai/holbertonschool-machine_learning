65ab2c6c1da57f1fd63bbaa9350b416c1dffa063
MurdoNicolai <murdo.nicolai.pro@gmail.com>
2023-07-28 18:28:08 +0200

    adding 100

---
+++ math/linear_algebra/100-slice_like_a_ninja.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+"""contains slice function"""
+import numpy as np
+
+
+def np_slice(matrix, axes={}):
+    """returns slice a matrix along a axis, axes is a dictionary where the key
+    is an axis to slice along and the value is a tuple representing the slice
+    to make along that axis"""
+
+
+    if "matrix_depth" not in axes:
+        axes["matrix_depth"] = matrix.ndim
+    if axes["matrix_depth"] - matrix.ndim in axes:
+        start = stop = step = None
+        if len(axes[axes["matrix_depth"] - matrix.ndim]) > 0:
+            start = axes[axes["matrix_depth"] - matrix.ndim][0]
+        if len(axes[axes["matrix_depth"] - matrix.ndim]) > 1:
+            stop = axes[axes["matrix_depth"] - matrix.ndim][1]
+        if len(axes[axes["matrix_depth"] - matrix.ndim]) > 2:
+            step = axes[axes["matrix_depth"] - matrix.ndim][2]
+        matrix = matrix[slice(start, stop, step)]
+
+
+
+    if type(matrix[0]) is not type(np.array([])):
+        return matrix
+
+    temp_matrix = []
+    for row_nb in range(len(matrix)):
+        temp_matrix.append(np_slice(matrix[row_nb], axes))
+
+    matrix = np.array(temp_matrix)
+
+    return matrix
