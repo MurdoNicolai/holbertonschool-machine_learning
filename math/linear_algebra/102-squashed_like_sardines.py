7f0282c5d48928753441d13f11ed3f91d45640aa
MurdoNicolai <murdo.nicolai.pro@gmail.com>
2023-07-28 20:34:13 +0200

    bugfix nont on lower dimention 102

--- math/linear_algebra/102-squashed_like_sardines.py
+++ math/linear_algebra/102-squashed_like_sardines.py
@@ -4,15 +4,18 @@
 
 def cat_matrices(mat1, mat2, axis=0):
     """concatenate matrices along the axis specified"""
+    result = []
     if axis == 0:
-        result = []
         for row_nb in range(len(mat1)):
             result.append(mat1[row_nb])
         for row_nb in range(len(mat2)):
             result.append(mat2[row_nb])
         return result
+    elif len(mat1) != len(mat2):
+        return None
     else:
         for row_nb in range(len(mat1)):
-            return(cat_matrices(mat1[row_nb], mat2[row_nb], axis - 1))
-
-    return result
+            if cat_matrices(mat1[row_nb], mat2[row_nb], axis - 1) is None:
+                return None
+            result.append(cat_matrices(mat1[row_nb], mat2[row_nb], axis - 1))
+        return(result)
