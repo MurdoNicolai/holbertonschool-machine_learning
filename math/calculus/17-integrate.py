79cd29ccec141dc587b03086ce806d5164cca97a
MurdoNicolai <murdo.nicolai.pro@gmail.com>
2023-08-04 09:33:55 +0200

    change 17

--- math/calculus/17-integrate.py
+++ math/calculus/17-integrate.py
@@ -4,7 +4,7 @@
 
 def poly_integral(poly, C=0):
     """Returns the integral of the given polynomial"""
-    new_poly = []
+    new_poly = [C]
     position =  1
     for value in poly:
         if type(value) is not int and type(value) is not float:
@@ -14,5 +14,4 @@ def poly_integral(poly, C=0):
             result = int(result)
         new_poly.append(result)
         position += 1
-    new_poly.append(C)
     return new_poly
