6b7b4df714299fd66385a5c6fc463c68d5f52169
MurdoNicolai <murdo.nicolai.pro@gmail.com>
2023-08-22 18:48:15 +0200

    add 1

---
+++ supervised_learning/classification/1-neuron.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""contains neuron class"""
+import numpy as np
+
+
+class Neuron():
+    """ deffines the neuron for a network"""
+    def __init__(self, nx):
+        """
+        initialize the neuron
+        W: The weights vector for the neuron.
+        b: The bias for the neuron
+        A: The activated output of the neuron (prediction).
+        """
+        if type(nx) is not int:
+            raise TypeError("nx must be an integer")
+        elif nx < 1:
+            raise ValueError("nx must be a positive integer")
+        else:
+            self._W = np.random.randn(1, nx)
+            self._b = 0
+            self._A = 0
