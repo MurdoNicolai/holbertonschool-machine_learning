505d6cc1eb04ce239d638e1199066d288364c41b
MurdoNicolai <murdo.nicolai.pro@gmail.com>
2023-08-23 11:24:18 +0200

    adding 2

---
+++ supervised_learning/classification/2-neuron.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+"""contains neuron class"""
+import numpy as np
+
+
+class Neuron():
+    """ deffines the neuron for a network"""
+    def __init__(self, nx):
+        """
+        initialize the neuron
+        W: The weights vector for the neuron.
+        b: The bias for the neuron
+        A: The activated output of the neuron (prediction).
+        """
+        if type(nx) is not int:
+            raise TypeError("nx must be an integer")
+        elif nx < 1:
+            raise ValueError("nx must be a positive integer")
+        else:
+            self._W = np.random.randn(1, nx)
+            self._b = 0
+            self._A = 0
+
+    @property
+    def W(self):
+        """I'm the 'W' property."""
+        return self._W
+
+    @property
+    def b(self):
+        """I'm the 'b' property."""
+        return self._b
+
+    @property
+    def A(self):
+        """I'm the 'A' property."""
+        return self._A
+
+    def forward_prop(self, X):
+        """Calculates the forward propagation of the neuron"""
+        self._A = self._W
+        for i in self._A:
+            for j in i:
+                j = 1 / (1 + np.exp(-self._A))
+        return self._A
