dc95db67534c6dba06ef00222519a7971d29eaa4
MurdoNicolai <murdo.nicolai.pro@gmail.com>
2023-08-26 14:02:10 +0200

    add 25 encode

---
+++ supervised_learning/classification/25-one_hot_decode.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""contains one_hot_encode function"""
+import numpy as np
+
+
+def one_hot_decode(one_hot):
+    """converts one_hot matrix into numerical base vector"""
+    if type(one_hot) is not np.ndarray:
+        return None
+    nb_in_row = len(one_hot[0])
+    numbers = np.array(range(0, nb_in_row))
+    return np.matmul(numbers, one_hot.astype(int))
--- supervised_learning/classification/test.py
+++ supervised_learning/classification/test.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python3
 
 import numpy as np
-oh_encode = __import__('24-one_hot_encode').one_hot_encode
 
-np.random.seed(3)
-classes = np.random.randint(0, 20)
-m = np.random.randint(100, 200)
-Y = np.random.randint(0, classes, m)
-np.set_printoptions(threshold=np.inf)
-print(oh_encode(Y, 'classes'))
+oh_encode = __import__('24-one_hot_encode').one_hot_encode
+oh_decode = __import__('25-one_hot_decode').one_hot_decode
+
+lib = np.load('../data/MNIST.npz')
+Y = lib['Y_train'][:10]
+
+print(Y)
+Y_one_hot = oh_encode(Y, 10)
+Y_decoded = oh_decode(Y_one_hot)
+print(Y_decoded)
