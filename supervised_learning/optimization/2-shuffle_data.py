1dbcf622447aea0a50d857c27e6b41ce2cec8afd
MurdoNicolai <murdo.nicolai.pro@gmail.com>
2023-08-29 16:38:52 +0200

    fix 2 to shuffle identicaly

--- supervised_learning/optimization/2-shuffle_data.py
+++ supervised_learning/optimization/2-shuffle_data.py
@@ -5,4 +5,7 @@ import numpy as np
 
 def shuffle_data(X, Y):
     """" that shuffles the data points in two matrices the same way"""
-    return(np.random.permutation(X), np.random.permutation(Y))
+    fuse = np.concatenate((X, Y), axis=1)
+    shuffle = np.random.permutation(fuse)
+    X, Y = np.split(shuffle, 2, axis=1)
+    return(X, Y)
