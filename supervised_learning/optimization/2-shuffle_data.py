6aa0c4667c7b9c4feaa7e241e8923ea5707d2521
MurdoNicolai <murdo.nicolai.pro@gmail.com>
2023-08-29 16:55:14 +0200

    make sure the code shuffle correctly

--- supervised_learning/optimization/2-shuffle_data.py
+++ supervised_learning/optimization/2-shuffle_data.py
@@ -5,7 +5,11 @@ import numpy as np
 
 def shuffle_data(X, Y):
     """" that shuffles the data points in two matrices the same way"""
+    print(X[0], Y[0])
+    lenX = X[0].size
     fuse = np.concatenate((X, Y), axis=1)
     shuffle = np.random.permutation(fuse)
-    X, Y = np.split(shuffle, 2, axis=1)
+    split = np.split(shuffle, shuffle[0].size, axis=1)
+    X = np.concatenate(split[:lenX], axis=1)
+    Y = np.concatenate(split[lenX:], axis=1)
     return(X, Y)
--- supervised_learning/optimization/test.py
+++ supervised_learning/optimization/test.py
@@ -3,20 +3,9 @@
 import numpy as np
 shuffle_data = __import__('2-shuffle_data').shuffle_data
 
-if __name__ == '__main__':
-    X = np.array([[1, 2],
-                [3, 4],
-                [5, 6],
-                [7, 8],
-                [9, 10]])
-    Y = np.array([[11, 12],
-                [13, 14],
-                [15, 16],
-                [17, 18],
-                [19, 20]])
+np.random.seed(2)
 
-    np.random.seed(0)
-    X_shuffled, Y_shuffled = shuffle_data(X, Y)
-
-    print(X_shuffled)
-    print(Y_shuffled)
+m, nx, ny = np.random.randint(10, 100, 3)
+X = np.random.randn(m, nx)
+Y = np.random.randn(m, ny)
+print(shuffle_data(X, Y))
