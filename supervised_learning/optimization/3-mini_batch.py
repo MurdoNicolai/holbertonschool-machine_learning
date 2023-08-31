8ffb8e0cade5578ef12d99622ed064fb250c97ad
MurdoNicolai <murdo.nicolai.pro@gmail.com>
2023-08-31 13:00:52 +0200

    delete useless import

--- supervised_learning/optimization/3-mini_batch.py
+++ supervised_learning/optimization/3-mini_batch.py
@@ -3,7 +3,6 @@
 import numpy as np
 import tensorflow.compat.v1 as tf
 shuffle_data = __import__('2-shuffle_data').shuffle_data
-create_train_op = __import__('5-create_train_op').create_train_op
 
 
 def train_mini_batch(X_train, Y_train, X_valid, Y_valid, batch_size=32, epochs=5, load_path="/tmp/model.ckpt", save_path="/tmp/model.ckpt"):
