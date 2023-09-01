86e32a004870c6bcaa24fff4e9c58d90db0a5e7d
MurdoNicolai <murdo.nicolai.pro@gmail.com>
2023-09-01 13:55:40 +0200

    see if dif online

--- supervised_learning/optimization/3-mini_batch.py
+++ supervised_learning/optimization/3-mini_batch.py
@@ -35,6 +35,21 @@ def train_mini_batch(X_train, Y_train, X_valid, Y_valid, batch_size=32,
         m = X_train.shape[0]
         for epoch in range(epochs):
             X_train, Y_train = shuffle_data(X_train, Y_train)
+
+            for step in range(0, m - 1, batch_size):
+                end = step + batch_size
+                X_batch = X_train[step:end]
+                Y_batch = Y_train[step:end]
+
+                # if step % (100 * batch_size) == 0 and (step != 0):
+                #     print("\tStep {}:\n\t\tCost: {}\n\t\tAccuracy: {}".format(
+                #           int(step/batch_size), step_cost, step_accuracy))
+                sess.run(train_op, feed_dict={x: X_batch, y: Y_batch})
+                step_cost = sess.run(loss,
+                                     feed_dict={x: X_batch, y: Y_batch})
+                step_accuracy = sess.run(accuracy,
+                                         feed_dict={x: X_batch, y: Y_batch})
+
             train_cost = sess.run(loss,
                                   feed_dict={x: X_train, y: Y_train})
             train_accuracy = sess.run(accuracy,
@@ -50,19 +65,15 @@ def train_mini_batch(X_train, Y_train, X_valid, Y_valid, batch_size=32,
             print("\tValidation Cost: {}\n\tValidation Accuracy: {}".format(
                   valid_cost, valid_accuracy))
 
-            for step in range(0, m, batch_size):
-                end = step + batch_size
-                X_batch = X_train[step:end]
-                Y_batch = Y_train[step:end]
+            train_cost, train_accuracy = sess.run([loss, accuracy], feed_dict={x: X_train, y: Y_train})
+            valid_cost, valid_accuracy = sess.run([loss, accuracy], feed_dict={x: X_valid, y: Y_valid})
 
-                if step % (100 * batch_size) == 0 and (step != 0):
-                    print("\tStep {}:\n\t\tCost: {}\n\t\tAccuracy: {}".format(
-                          int(step/batch_size), step_cost, step_accuracy))
-                sess.run(train_op, feed_dict={x: X_batch, y: Y_batch})
-                step_cost = sess.run(loss,
-                                     feed_dict={x: X_batch, y: Y_batch})
-                step_accuracy = sess.run(accuracy,
-                                         feed_dict={x: X_batch, y: Y_batch})
+            print("After {} epochs:".format(epoch + 1))
+            print("\tTraining Cost: {}\n\tTraining Accuracy: {}".format(
+                  train_cost, train_accuracy))
+            print("\tValidation Cost: {}\n\tValidation Accuracy: {}".format(
+                  valid_cost, valid_accuracy))
+            return 0
 
         saver.save(sess, save_path)
 
