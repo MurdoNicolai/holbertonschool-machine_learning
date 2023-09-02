3e5befec845116a989381009c3970d67b7d0a49a
MurdoNicolai <murdo.nicolai.pro@gmail.com>
2023-09-02 19:11:14 +0200

    add 5

---
+++ supervised_learning/optimization/5-momentum.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+""" moving average"""
+import numpy as np
+
+
+def update_variables_momentum(alpha, beta1, var, grad, v):
+    """ updates a variable using the gradient descent
+        with momentum optimization algorithm """
+    v_new = ((beta1 * v) + ((1 - beta1) * grad))
+    var_new = var + v_new * alpha
+
+    return var_new, v_new
