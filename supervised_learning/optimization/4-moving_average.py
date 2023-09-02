f4a2eb0c35d710d57778d781beb02b23a1d2e9ea
MurdoNicolai <murdo.nicolai.pro@gmail.com>
2023-09-02 18:50:49 +0200

    add4

---
+++ supervised_learning/optimization/4-moving_average.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+""" contains normalization"""
+import tensorflow.compat.v1 as tf
+tf.disable_eager_execution()
+
+
+def moving_average(data, beta):
+    """ calculates moving average"""
+    Average = 0
+    total_numbers = 0
+    moving_average = []
+    for number in data:
+        total_numbers += 1
+        print((beta * Average), ((1 - beta) * number), (1 - beta**total_numbers))
+        Average = ((beta * Average) + ((1 - beta) * number))
+
+        moving_average.append(Average/(1 - beta ** total_numbers))
+    return moving_average
