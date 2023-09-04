0826361bb7b7350257eab7af587a0546f13f2a61
MurdoNicolai <murdo.nicolai.pro@gmail.com>
2023-09-04 18:57:09 +0200

    using module 1 adn 2

--- supervised_learning/error_analysis/4-f1_score.py
+++ supervised_learning/error_analysis/4-f1_score.py
@@ -1,14 +1,11 @@
 #!/usr/bin/env python3
 """contains f1_score"""
 import numpy as np
+sensitivity = __import__('1-sensitivity').sensitivity
+precision = __import__('2-precision').precision
 
 
 def f1_score(confusion):
     """retrun f1_score of confusion matrix"""
-    result = np.zeros(len(confusion))
-    for nb in range(len(confusion)):
-        positivs = confusion[nb][nb]
-        supposed_positivs = np.sum(confusion.T[nb])
-        actual_positivs = np.sum(confusion[nb])
-        result[nb] = 2 * positivs/(supposed_positivs + actual_positivs)
+    result = 2/(1/sensitivity(confusion) + 1/precision(confusion))
     return result
