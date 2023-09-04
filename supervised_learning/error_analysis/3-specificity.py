b23d6737ca9538136faae971bcc8ded992d15149
MurdoNicolai <murdo.nicolai.pro@gmail.com>
2023-09-04 18:45:29 +0200

    fix specificity

--- supervised_learning/error_analysis/3-specificity.py
+++ supervised_learning/error_analysis/3-specificity.py
@@ -7,12 +7,13 @@ def specificity(confusion):
     """retrun specificity of confusion matrix"""
     result = np.zeros(len(confusion))
     all_accurate = 0
+    all = np.sum(confusion)
     for nb in range(len(confusion)):
         all_accurate += confusion[nb][nb]
     for nb in range(len(confusion)):
-        true_negatives = all_accurate - confusion[nb][nb]
-        positivs = confusion[nb][nb]
-        supposed_positivs = np.sum(confusion[nb])
-        all_negatives = supposed_positivs - positivs + true_negatives
-        result[nb] = true_negatives/all_negatives
+        true_negatives = (all + confusion[nb][nb]
+                          - np.sum(confusion[nb]) - np.sum(confusion.T[nb]))
+        true_positivs = confusion[nb][nb]
+        false_postives = np.sum(confusion.T[nb]) - true_positivs
+        result[nb] = true_negatives/(false_postives + true_negatives)
     return result
--- supervised_learning/error_analysis/test.py
+++ supervised_learning/error_analysis/test.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 
 import numpy as np
-sensitivity = __import__('1-sensitivity').sensitivity
+specificity = __import__('3-specificity').specificity
 
 if __name__ == '__main__':
     confusion = np.load('confusion.npz')['confusion']
 
     np.set_printoptions(suppress=True)
-    print(sensitivity(confusion))
+    print(specificity(confusion))
