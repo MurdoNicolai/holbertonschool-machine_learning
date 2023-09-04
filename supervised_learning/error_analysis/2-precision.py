12df581cb8f4a75b1223bb3f8212ddb006dae085
MurdoNicolai <murdo.nicolai.pro@gmail.com>
2023-09-04 17:53:53 +0200

    add 2

---
+++ supervised_learning/error_analysis/2-precision.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""contains precision"""
+import numpy as np
+
+
+def precision(confusion):
+    """retrun precision of confusion matrix"""
+    result = np.zeros(len(confusion))
+    for nb in range(len(confusion)):
+        positivs = confusion[nb][nb]
+        supposed_positivs = np.sum(confusion.T[nb])
+        result[nb] = positivs/predicted_positivs
+    return result
