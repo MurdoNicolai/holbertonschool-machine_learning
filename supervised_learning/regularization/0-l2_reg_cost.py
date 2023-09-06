25ac25c7599e658b0ff2b3e12ed00c70e59141e8
MurdoNicolai <murdo.nicolai.pro@gmail.com>
2023-09-06 15:56:39 +0200

    select only weight not bieases

--- supervised_learning/regularization/0-l2_reg_cost.py
+++ supervised_learning/regularization/0-l2_reg_cost.py
@@ -6,6 +6,7 @@ import numpy as np
 def l2_reg_cost(cost, lambtha, weights, L, m):
     """ calculates the cost of a neural network with L2 regularization"""
     weights_sum = 0
-    for value in weights.values():
-        weights_sum += np.sum(np.multiply(value, value))
+    for key, value in weights.items():
+        if key[:1] == 'W':
+            weights_sum += np.sum(np.multiply(value, value))
     return (weights_sum/(2*m) * lambtha + cost)
