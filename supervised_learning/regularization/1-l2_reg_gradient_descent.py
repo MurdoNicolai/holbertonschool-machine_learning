d81fbdd9b4c2516ab4e6f550425fc817b21883a7
MurdoNicolai <murdo.nicolai.pro@gmail.com>
2023-09-06 17:27:18 +0200

    test 1

--- supervised_learning/regularization/1-l2_reg_gradient_descent.py
+++ supervised_learning/regularization/1-l2_reg_gradient_descent.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
-"""containst reg_cost functino"""
+"""containst gradient_descent functino"""
 import numpy as np
 import tensorflow.compat.v1 as tf
 
 
-def l2_reg_cost(cost, lambtha, weights, L, m):
-    """ calculates the cost of a neural network with L2 regularization"""
+def l2_reg_gradient_descent(Y, weights, cache, alpha, lambtha, L):
+    """ updates the weights and biases of a neural network
+    using gradient descent with L2 regularization"""
     weights_sum = 0
     for key, value in weights.items():
         if key[:1] == 'W':
