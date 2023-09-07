d5114e7cf8803772c163f66b24a2d2fc14bc66f0
MurdoNicolai <murdo.nicolai.pro@gmail.com>
2023-09-07 17:42:08 +0200

    don't sum

--- supervised_learning/regularization/2-l2_reg_cost.py
+++ supervised_learning/regularization/2-l2_reg_cost.py
@@ -6,13 +6,7 @@ import tensorflow.compat.v1 as tf
 def l2_reg_cost(cost):
     """ calculates the cost of a neural network with L2 regularization"""
     reg_losses = tf.compat.v1.losses.get_regularization_losses()
-    print (cost)
-    print(reg_losses)
-    print(tf.add_n(reg_losses))
-    # Sum up the regularization losses
-    total_reg_loss = tf.add_n(reg_losses)
 
-    # Add regularization to the original loss
-    total_loss = cost + total_reg_loss
+    total_loss = cost + reg_losses
 
     return total_loss
