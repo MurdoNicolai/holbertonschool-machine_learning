ae3caf325a71e7e6b35a9ce5b5d4ba4cfe513332
MurdoNicolai <murdo.nicolai.pro@gmail.com>
2023-09-07 17:36:00 +0200

    remove sum

--- supervised_learning/regularization/2-l2_reg_cost.py
+++ supervised_learning/regularization/2-l2_reg_cost.py
@@ -6,9 +6,7 @@ import tensorflow.compat.v1 as tf
 def l2_reg_cost(cost):
     """ calculates the cost of a neural network with L2 regularization"""
     trainable_vars = tf.trainable_variables()
-    print(trainable_vars, cost)
-    print([tf.nn.l2_loss(var) for var in trainable_vars])
-    l2_reg_term = tf.add_n([tf.nn.l2_loss(var) for var in trainable_vars])
+    l2_reg_term = [tf.nn.l2_loss(var) for var in trainable_vars]
 
     cost_with_l2_reg = cost + (1 / 2.0) * l2_reg_term
 
