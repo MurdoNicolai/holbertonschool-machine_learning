632d1de41ef541315d955a56f5e794c6168e4a7a
MurdoNicolai <murdo.nicolai.pro@gmail.com>
2023-09-08 11:54:05 +0200

    testy 3

--- supervised_learning/regularization/3-l2_reg_create_layer.py
+++ supervised_learning/regularization/3-l2_reg_create_layer.py
@@ -15,5 +15,5 @@ def l2_reg_create_layer(prev, n, activation, lambtha):
                                   keras.initializers.
                                   VarianceScaling(scale=2.0, mode=("fan_avg")),
                                   kernel_regularizer=tf.
-                                  keras.regularizers.L2(lambtha))(prev)
+                                  keras.regularizers.L2(lambtha))
     return dense_layer
