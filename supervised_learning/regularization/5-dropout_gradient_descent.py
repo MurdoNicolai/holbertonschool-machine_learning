0c494293d513ea71fd74683b3f606b2505e4b445
MurdoNicolai <murdo.nicolai.pro@gmail.com>
2023-09-08 13:04:32 +0200

    corrected 5 dropout with forward propagation

--- supervised_learning/regularization/5-dropout_gradient_descent.py
+++ supervised_learning/regularization/5-dropout_gradient_descent.py
@@ -9,15 +9,17 @@ def dropout_gradient_descent(Y, weights, cache, alpha, keep_prob, L):
     dZ = A - Y
     newweights = {}
     for i in range(L, 0, -1):
+        if i != L:
+            D = cache["D{}".format(i)]
+            dZ = dZ * D / keep_prob
         A_prev = cache["A{}".format(i - 1)]
-
         dW = np.matmul(dZ, A_prev .T)/len(A_prev[0])
         db = np.sum(dZ, axis=1, keepdims=True) / len(A_prev[0])
         da = np.matmul(weights["W{}".format(i)].T, dZ)
-        newweights.update({"W{}".format(i):
-                          weights["W{}".format(i)] - alpha * dW})
-        newweights.update({"b{}".format(i):
-                          weights["b{}".format(i)] - alpha * db})
+        new_W = weights["W{}".format(i)] - alpha * dW
+        new_b = weights["b{}".format(i)] - alpha * db
+        newweights.update({"W{}".format(i): new_W})
+        newweights.update({"b{}".format(i): new_b})
 
         dg2 = 1 - A_prev ** 2
         dZ = (da) * dg2
