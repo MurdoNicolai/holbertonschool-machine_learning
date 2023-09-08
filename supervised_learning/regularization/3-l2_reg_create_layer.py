979b2dd93a372c594d9195bda1d4904c3f068e7c
MurdoNicolai <murdo.nicolai.pro@gmail.com>
2023-09-08 11:13:12 +0200

    put back orriginal code

--- supervised_learning/regularization/3-l2_reg_create_layer.py
+++ supervised_learning/regularization/3-l2_reg_create_layer.py
@@ -10,25 +10,7 @@ def l2_reg_create_layer(prev, n, activation, lambtha):
     Returns:
     The output of the new layer.
     """
-    layer_name = "layer_with_l2_reg"
-
-    # Define the weight matrix with L2 regularization
-    with tf.variable_scope(layer_name):
-        initializer = tf.random_normal_initializer(stddev=0.1)
-        weights = tf.get_variable("weights", shape=[int(prev.get_shape()[1]), n],
-                                  initializer=initializer,
-                                  regularizer=tf.keras.regularizers.L2(lambtha))
-
-        # Define the bias vector
-        bias = tf.get_variable("bias", shape=[n], initializer=tf.zeros_initializer())
-
-    # Linear combination of inputs and weights
-    z = tf.matmul(prev, weights) + bias
-
-    # Apply activation function
-    if activation is not None:
-        dense_layer = activation(z)
-    else:
-        dense_layer = z
-
+    dense_layer = tf.layers.dense(prev, units=n, activation=activation,
+                                  kernel_regularizer=tf.keras.
+                                  regularizers.L2(L2=lambtha))
     return dense_layer
