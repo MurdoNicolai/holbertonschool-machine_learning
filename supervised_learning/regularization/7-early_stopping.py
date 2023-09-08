c27ee93b2eeadeda2b2ea661fb6cdf98e7e3bdad
MurdoNicolai <murdo.nicolai.pro@gmail.com>
2023-09-08 13:49:18 +0200

    pycodevalidate

--- supervised_learning/regularization/7-early_stopping.py
+++ supervised_learning/regularization/7-early_stopping.py
@@ -6,7 +6,7 @@ def early_stopping(cost, opt_cost, threshold, patience, count):
     """
     determines if you should stop gradient descent early
     """
-    if  opt_cost - cost <= threshold:
+    if opt_cost - cost <= threshold:
         count += 1
     else:
         count = 0
