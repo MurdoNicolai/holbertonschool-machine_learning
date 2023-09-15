6951719a26f7d727c2579e23d07e9649fbeef214
MurdoNicolai <murdo.nicolai.pro@gmail.com>
2023-09-15 15:08:21 +0200

    add return to 0

--- supervised_learning/keras/0-sequential.py
+++ supervised_learning/keras/0-sequential.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import numpy as np
 import tensorflow.keras as K
 
 
@@ -17,3 +18,4 @@ def build_model(nx, layers, activations, lambtha, keep_prob):
             model.add(K.layers.Dropout(1 - keep_prob))
         else:
             model.add(K.layers.Dense(layer, activation=activation))
+    return model
--- supervised_learning/keras/test.py
+++ supervised_learning/keras/test.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
-import tensorflow.keras as K
+
+
 build_model = __import__('0-sequential').build_model
 
 model = build_model(200, [100, 50, 10], ['tanh', 'sigmoid', 'softmax'], 0.01, 0.6)
