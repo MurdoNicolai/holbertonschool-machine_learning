30da45c2bcd5ab637a1e56705bc26c2c5dab83e2
MurdoNicolai <murdo.nicolai.pro@gmail.com>
2023-09-16 19:55:13 +0200

    redo save load( 11)

--- supervised_learning/keras/11-config.py
+++ supervised_learning/keras/11-config.py
@@ -7,8 +7,7 @@ def save_config(network, filename):
     """
     Saves a model's configuration to a file.
     """
-    with open(filename, 'w') as config_file:
-        config_file.write(network.to_yaml())
+    network.save(filename)
 
 
 def load_config(filename):
@@ -16,5 +15,5 @@ def load_config(filename):
     Loads a model with a specific configuration from a file.
     """
     with open(filename, 'r') as config_file:
-        loaded_model = K.models.model_from_yaml(config_file.read())
+        loaded_model = K.models.load_model(config_file.read())
     return loaded_model
