132a8dc36cf639c53356cc09a1fef78149472ac4
MurdoNicolai <murdo.nicolai.pro@gmail.com>
2023-09-16 19:46:29 +0200

    fix 12

--- supervised_learning/keras/12-test.py
+++ supervised_learning/keras/12-test.py
@@ -8,4 +8,4 @@ def test_model(network, data, labels, verbose=True):
     Tests a neural network model on a dataset.
     """
     loss, accuracy = network.evaluate(data, labels, verbose=verbose)
-    return loss, accuracy
+    return [loss, accuracy]
--- supervised_learning/keras/test.py
+++ supervised_learning/keras/test.py
@@ -1,13 +1,30 @@
 #!/usr/bin/env python3
+"""
+Main file
+"""
 
+# Force Seed - fix for Keras
+SEED = 0
+
+import os
+os.environ['PYTHONHASHSEED'] = str(SEED)
+import random
+random.seed(SEED)
+import numpy as np
+np.random.seed(SEED)
+import tensorflow as tf
+tf.random.set_seed(SEED)
 import tensorflow.keras as K
-build_model = __import__('0-sequential').build_model
 
-model = build_model(200, [100, 50, 10], ['tanh', 'sigmoid', 'softmax'], 0.01, 0.6)
-for layer in model.layers:
-    if type(layer) == K.layers.Dense:
-        try:
-            print(layer.kernel_regularizer.l1)
-        except AttributeError:
-            print(.0)
-        print(layer.kernel_regularizer.l2)
+# Imports
+model = __import__('9-model')
+config = __import__('11-config')
+
+if __name__ == '__main__':
+    network = model.load_model('network1.h5')
+    config.save_config(network, 'config1.json')
+    del network
+
+    network2 = config.load_config('config1.json')
+    network2.summary()
+    print(network2.get_weights())
