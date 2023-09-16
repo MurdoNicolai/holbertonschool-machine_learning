505b3387c19a6228ee06c61fda0fedb680623c73
MurdoNicolai <murdo.nicolai.pro@gmail.com>
2023-09-16 18:56:01 +0200

    change to look more like 0

--- supervised_learning/keras/1-input.py
+++ supervised_learning/keras/1-input.py
@@ -9,14 +9,17 @@ def build_model(nx, layers, activations, lambtha, keep_prob):
     input_layer = K.layers.Input(shape=(nx,))
 
     x = input_layer
-
+    first_layer = True
     for layer, activation in zip(layers, activations):
-        x = K.layers.Dense(
-            layer, activation=activation,
-            kernel_regularizer=K.regularizers.l2(lambtha))(x)
-
-        x = K.layers.Dropout(1 - keep_prob)(x)
-
+        if first_layer:
+            x = K.layers.Dense(layer, activation=activation,
+                               kernel_regularizer=K.
+                               regularizers.l2(lambtha))(x)
+            first_layer = False
+        else:
+            x = K.layers.Dropout(1 - keep_prob)(x)
+            x = K.layers.Dense(layer, activation=activation,
+                               kernel_regularizer=K.
+                               regularizers.l2(lambtha))(x)
     model = K.models.Model(inputs=input_layer, outputs=x)
-
     return model
