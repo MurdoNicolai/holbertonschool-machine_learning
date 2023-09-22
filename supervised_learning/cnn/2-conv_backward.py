3a58b6938b3abf5013f14d001c7515386dec6e8c
MurdoNicolai <murdo.nicolai.pro@gmail.com>
2023-09-22 16:20:04 +0200

    test

--- supervised_learning/cnn/2-conv_backward.py
+++ supervised_learning/cnn/2-conv_backward.py
@@ -1,11 +1,6 @@
-#!/usr/bin/env python3
-""" conv backward propagation"""
 import numpy as np
 
 def conv_backward(dZ, A_prev, W, b, padding="same", stride=(1, 1)):
-    """
-    performs back propagation over a convolutional layer of a neural network
-    """
     m, h_new, w_new, c_new = dZ.shape
     kh, kw, c_prev, _ = W.shape
     sh, sw = stride
@@ -13,8 +8,7 @@ def conv_backward(dZ, A_prev, W, b, padding="same", stride=(1, 1)):
     if padding == "same":
         pad_h = ((h_new - 1) * sh + kh - A_prev.shape[1]) // 2
         pad_w = ((w_new - 1) * sw + kw - A_prev.shape[2]) // 2
-        A_prev_padded = np.pad(A_prev,
-                               ((0, 0), (pad_h, pad_h), (pad_w, pad_w), (0, 0)), mode='constant')
+        A_prev_padded = np.pad(A_prev, ((0, 0), (pad_h, pad_h), (pad_w, pad_w), (0, 0)), mode='constant')
     else:
         A_prev_padded = A_prev
 
@@ -31,13 +25,15 @@ def conv_backward(dZ, A_prev, W, b, padding="same", stride=(1, 1)):
                     horiz_start = w * sw
                     horiz_end = horiz_start + kw
 
-                    A_slice = A_prev_padded[i, vert_start:vert_end,
-                                            horiz_start:horiz_end, :]
-                    dA_prev[i, vert_start:vert_end,
-                            horiz_start:horiz_end, :] += W[:, :,
-                                                           :, f] * dZ[i, h,
-                                                                      w, f]
+                    if padding == "same":
+                        A_slice = A_prev_padded[i, vert_start:vert_end, horiz_start:horiz_end, :]
+                    else:
+                        A_slice = A_prev[i, vert_start:vert_end, horiz_start:horiz_end, :]
+
+                    dA_prev[i, vert_start:vert_end, horiz_start:horiz_end, :] += W[:, :, :, f] * dZ[i, h, w, f]
                     dW[:, :, :, f] += A_slice * dZ[i, h, w, f]
 
-    return dA_prev, dW, db
+    if padding == "valid":
+        dA_prev = dA_prev[:, kh - 1:-(kh - 1), kw - 1:-(kw - 1), :]
 
+    return dA_prev, dW, db
