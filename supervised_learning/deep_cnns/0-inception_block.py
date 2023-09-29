20b1e09e1a62a49536ca909890d59211883ba4cb
MurdoNicolai <murdo.nicolai.pro@gmail.com>
2023-09-29 15:44:34 +0200

    same change but ok

--- supervised_learning/deep_cnns/0-inception_block.py
+++ supervised_learning/deep_cnns/0-inception_block.py
@@ -16,10 +16,10 @@ def inception_block(A_prev, filters):
                               activation='relu')(conv3x3r)
 
     conv5x5r = K.layers.Conv2D(filters[3], (1, 1), padding='same',
-                              activation='relu')(conv5x5r)
+                              activation='relu')(x)
 
     conv5x5 = K.layers.Conv2D(filters[4], (5, 5), padding='same',
-                              activation='relu')(x)
+                              activation='relu')(conv5x5r)
 
     max_pool = K.layers.MaxPooling2D((3, 3), strides=(1, 1), padding='same')(x)
     max_pool_conv = K.layers.Conv2D(filters[5], (1, 1), padding='same',
