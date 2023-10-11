9f21a4fe610b05df1da4ebb756c0d1e592f9b6b9
MurdoNicolai <murdo.nicolai.pro@gmail.com>
2023-10-11 18:10:17 +0200

    test 60

--- supervised_learning/object_detection/1-yolo.py
+++ supervised_learning/object_detection/1-yolo.py
@@ -58,9 +58,9 @@ class Yolo():
             anchory = self.anchors[anchorcount][..., 1]
             anchory = anchory.reshape(1, 1, num_anchors, 1)
             widthx = (splt_out[0][..., 2:3] * anchorx
-                      * input_width / grid_width / 32)
+                      * input_width / grid_width / 60)
             widthy = (splt_out[0][..., 3:4] * anchory
-                      * input_height / grid_height / 32)
+                      * input_height / grid_height / 60)
 
             splt_out[0][..., :1] = centerx - widthx
             splt_out[0][..., 1:2] = centery - widthy
