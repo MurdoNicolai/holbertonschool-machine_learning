c31c94bd526686aead9910206dd843aa9dbe2389
MurdoNicolai <murdo.nicolai.pro@gmail.com>
2023-10-11 17:34:44 +0200

    add 1 yolo fix boxes

--- supervised_learning/object_detection/1-yolo.py
+++ supervised_learning/object_detection/1-yolo.py
@@ -14,7 +14,6 @@ def sigmoid(x):
     return 1 / (1 + np.exp(-x))
 
 
-
 class Yolo():
     """uses the Yolo v3 algorithm to perform object detection"""
     def __init__(self, model_path, classes_path, class_t, nms_t, anchors):
@@ -32,7 +31,6 @@ class Yolo():
         self.nms_t = nms_t
         self.anchors = anchors
 
-
     def process_outputs(self, outputs, image_size):
         """
         Returns a tuple of (boxes, box_confidences, box_class_probs):
@@ -43,30 +41,34 @@ class Yolo():
         boxes = []
         box_confidences = []
         box_class_probs = []
-        ih = image_size[0]
-        iw = image_size[1]
-        print(ih, iw)
-        for output in outputs:
-            output[..., :2] = 1.0 / (1.0 + np.exp(-output[..., :2]))
+        input_height = image_size[0]
+        input_width = image_size[1]
+        anchorcount = 0
+        for output in outputs[0:1]:
             output[..., 4] = 1.0 / (1.0 + np.exp(-output[..., 4]))
-            gh = output.shape[0]
-            gw = output.shape[1]
-            split_output = np.array_split(output, (4, 5, ), axis=3)
+            splt_out = np.array_split(output, (4, 5, ), axis=3)
+            grid_height, grid_width, num_anchors, _ = output.shape
 
+            splt_out[0][..., :2] = 1.0 / (1.0 + np.exp(-splt_out[0][..., :2]))
+            splt_out[0][..., 2:4] = np.exp(splt_out[0][..., 2:4])
+            centerx = splt_out[0][..., :1] * input_width / grid_width
+            centery = splt_out[0][..., 1:2] * input_height / grid_height
+            anchorx = self.anchors[anchorcount][..., 0]
+            anchorx = anchorx.reshape(1, 1, num_anchors, 1)
+            anchory = self.anchors[anchorcount][..., 1]
+            anchory = anchory.reshape(1, 1, num_anchors, 1)
+            widthx = (splt_out[0][..., 2:3] * anchorx
+                      * input_width / grid_width / 64)
+            widthy = (splt_out[0][..., 3:4] * anchory
+                      * input_height / grid_height / 64)
 
+            splt_out[0][..., :1] = centerx - widthx
+            splt_out[0][..., 1:2] = centery - widthy
+            splt_out[0][..., 2:3] = centerx + widthx
+            splt_out[0][..., 3:4] = centery + widthy
 
-
-            test = split_output[0][0][0][0]
-            answer = [-2.13743365e+02, -4.85478868e+02,  3.05682061e+02, 5.31534670e+02]
-            print(test)
-            print(answer[2]- answer[0])
-            print((answer[2] - answer[0]))
-            print((answer[3] - answer[1]))
-
-
-
-
-
-            box_confidences.append(split_output[1])
-            box_class_probs.append(sigmoid(split_output[2]))
+            boxes.append(splt_out[0])
+            box_confidences.append(splt_out[1])
+            box_class_probs.append(sigmoid(splt_out[2]))
+            anchorcount += 1
         return ((boxes, box_confidences, box_class_probs))
