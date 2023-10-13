e5416828428945e18bc2f1d1a3517db246485626
MurdoNicolai <murdo.nicolai.pro@gmail.com>
2023-10-13 09:48:23 +0200

    add correct imports

--- supervised_learning/deep_cnns/4-resnet50.py
+++ supervised_learning/deep_cnns/4-resnet50.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 """to make"""
 import tensorflow.keras as K
+identity_block = __import__('2-identity_block').identity_block
+projection_block = __import__('3-projection_block').projection_block
 
 
 def resnet50():
