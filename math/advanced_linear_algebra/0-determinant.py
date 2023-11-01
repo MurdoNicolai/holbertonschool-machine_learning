92c82903d1112bf86f0db37be9f943bd7f1129e7
MurdoNicolai <murdo.nicolai.pro@gmail.com>
2023-11-01 17:53:43 +0100

    delete numpy

--- math/advanced_linear_algebra/0-determinant.py
+++ math/advanced_linear_algebra/0-determinant.py
@@ -1,30 +1,30 @@
 #!/usr/bin/env python3
 """contains matrix math functions"""
-import numpy as np
 
 
 def determinant(matrix):
     """ calculates the determinant of a matrix """
     if matrix == [[]]:
         return 0
-    try:
-        matrix = np.array(matrix)
-        if matrix.ndim != 2:
-            raise TypeError('matrix must be a list of lists')
-    except TypeError:
+    if (type(matrix) != list or
+        type(matrix[0]) != list or
+        type(matrix[0][0]) != int):
         raise TypeError('matrix must be a list of lists')
-    if matrix.shape[0] != matrix.shape[1]:
+    if len(matrix[0]) != len(matrix):
         raise ValueError('matrix must be a square matrix')
 
-    if matrix.shape[0] == 2:
+    if len(matrix) == 2:
         return matrix[0][0]*matrix[1][1] - matrix[1][0]*matrix[0][1]
-    if matrix.shape[0] == 1:
+    if len(matrix) == 1:
         return matrix[0][0]
 
     det = 0
     for i in range(len(matrix[0])):
-        temp_matrix = matrix
-        temp_matrix = np.delete(temp_matrix, 0, 0)
-        temp_matrix = np.delete(temp_matrix, i, 1)
+        temp_matrix = matrix.copy()
+        del temp_matrix[0]
+        for row in range(len(temp_matrix)):
+            temp_matrix[row] = temp_matrix[row].copy()
+        for row in temp_matrix:
+            del row[i]
         det += (matrix[0][i] * (-1)**i) * determinant(temp_matrix)
     return det
