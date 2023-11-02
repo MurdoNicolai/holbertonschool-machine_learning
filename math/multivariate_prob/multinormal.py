c5505d292ca89f4f991de589eee42ebb0c08a1a9
MurdoNicolai <murdo.nicolai.pro@gmail.com>
2023-11-02 18:04:54 +0100

    data transpose

--- math/multivariate_prob/multinormal.py
+++ math/multivariate_prob/multinormal.py
@@ -6,9 +6,9 @@ import numpy as np
 def mean_cov(X):
     """calculates the mean and covariance of a data set"""
     if type(X) is not np.ndarray or X.ndim != 2:
-        raise TypeError("X must be a 2D numpy.ndarray")
+        raise TypeError("data must be a 2D numpy.ndarray")
     if X.shape[0] < 2:
-        raise ValueError("X must contain multiple data points")
+        raise ValueError("data must contain multiple data points")
     mean = np.mean(X, axis=0, keepdims=True)
     centered_data = X - mean
     cov = np.dot(centered_data.T, centered_data) / (X.shape[0] - 1)
@@ -24,4 +24,4 @@ class MultiNormal:
             n is the number of data points
             d is the number of dimensions in each data point
         """
-        self.mean, self.cov = mean_cov(data)
+        self.mean, self.cov = mean_cov(data.T)
