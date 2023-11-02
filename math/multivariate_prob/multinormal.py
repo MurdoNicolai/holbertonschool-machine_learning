571e7606d77d125e66baa16847e5233e3735e4b4
MurdoNicolai <murdo.nicolai.pro@gmail.com>
2023-11-02 18:07:16 +0100

    fix minor issus with init

--- math/multivariate_prob/multinormal.py
+++ math/multivariate_prob/multinormal.py
@@ -5,14 +5,10 @@ import numpy as np
 
 def mean_cov(X):
     """calculates the mean and covariance of a data set"""
-    if type(X) is not np.ndarray or X.ndim != 2:
-        raise TypeError("data must be a 2D numpy.ndarray")
-    if X.shape[0] < 2:
-        raise ValueError("data must contain multiple data points")
     mean = np.mean(X, axis=0, keepdims=True)
     centered_data = X - mean
     cov = np.dot(centered_data.T, centered_data) / (X.shape[0] - 1)
-    return mean, cov
+    return mean.T, cov
 
 
 class MultiNormal:
@@ -24,4 +20,8 @@ class MultiNormal:
             n is the number of data points
             d is the number of dimensions in each data point
         """
+        if type(data) is not np.ndarray or data.ndim != 2:
+            raise TypeError("data must be a 2D numpy.ndarray")
+        if data.shape[0] < 2:
+            raise ValueError("data must contain multiple data points")
         self.mean, self.cov = mean_cov(data.T)
