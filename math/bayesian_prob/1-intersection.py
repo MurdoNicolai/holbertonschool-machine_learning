07c22fa971b5697fdfc879a9d168dc5bb648fce1
MurdoNicolai <murdo.nicolai.pro@gmail.com>
2023-11-09 12:43:26 +0100

    add shape check

--- math/bayesian_prob/1-intersection.py
+++ math/bayesian_prob/1-intersection.py
@@ -51,6 +51,9 @@ def intersection(x, n, P, Pr):
     if not all(0 <= p <= 1 for p in P):
         raise ValueError("All values in P must be in the range [0, 1]")
 
+    if not isinstance(Pr, np.ndarray) or Pr.shape!= P.shape:
+        raise TypeError("Pr must be a numpy.ndarray with the same shape as P")
+
     for pr in Pr:
         if not isinstance(pr, np.float64):
             raise ValueError("All values in Pr must be in the range [0, 1]")
