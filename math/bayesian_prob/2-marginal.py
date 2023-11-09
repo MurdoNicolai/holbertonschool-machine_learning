e451746c62d5773ee1b484664f97d447d2bb88e4
MurdoNicolai <murdo.nicolai.pro@gmail.com>
2023-11-09 13:01:51 +0100

    fix issues with calc

--- math/bayesian_prob/2-marginal.py
+++ math/bayesian_prob/2-marginal.py
@@ -11,20 +11,6 @@ def binomial_coefficient(n, k):
 
 def likelihood(x, n, P):
     """return likelihood of x positive in n event for each P probability"""
-    if not isinstance(n, int) or n <= 0:
-        raise ValueError("n must be a positive integer")
-
-    VE = "x must be an integer that is greater than or equal to 0"
-    if not isinstance(x, int) or x < 0:
-        raise ValueError(VE)
-    if x > n:
-        raise ValueError("x cannot be greater than n")
-
-    if not isinstance(P, np.ndarray) or P.ndim != 1:
-        raise TypeError("P must be a 1D numpy.ndarray")
-
-    if not all(0 <= p <= 1 for p in P):
-        raise ValueError("All values in P must be in the range [0, 1]")
 
     # Calculate the binomial probability mass function
     likelihoods = np.array([binomial_coefficient(n, x) * (p ** x) *
@@ -35,33 +21,6 @@ def likelihood(x, n, P):
 
 def intersection(x, n, P, Pr):
     """return intersection of x positive in n event for each P probability"""
-
-    if not isinstance(n, int) or n <= 0:
-        raise ValueError("n must be a positive integer")
-
-    VE = "x must be an integer that is greater than or equal to 0"
-    if not isinstance(x, int) or x < 0:
-        raise ValueError(VE)
-    if x > n:
-        raise ValueError("x cannot be greater than n")
-
-    if not isinstance(P, np.ndarray) or P.ndim != 1:
-        raise TypeError("P must be a 1D numpy.ndarray")
-
-    if not all(0 <= p <= 1 for p in P):
-        raise ValueError("All values in P must be in the range [0, 1]")
-
-    if not isinstance(Pr, np.ndarray) or Pr.shape != P.shape:
-        raise TypeError("Pr must be a numpy.ndarray with the same shape as P")
-
-    for pr in Pr:
-        if not isinstance(pr, np.float64):
-            raise ValueError("All values in Pr must be in the range [0, 1]")
-        if not (0 <= pr <= 1):
-            raise ValueError("All values in Pr must be in the range [0, 1]")
-
-    if not np.isclose(np.sum(Pr), 1.0):
-        raise ValueError("Pr must sum to 1")
 
     intersection_values = np.array([likelihood(x, n, np.array([p]))[0] * pr
                                     for p, pr in zip(P, Pr)])
@@ -97,7 +56,7 @@ def marginal(x, n, P, Pr):
 
     if not np.isclose(np.sum(Pr), 1.0):
         raise ValueError("Pr must sum to 1")
-    return (np.sum([intersection(x, n, P, Pr_i) for Pr_i in Pr]))
+    return (np.sum([intersection(x, n, P, Pr)]))
 
 def posterior(x, n, P, Pr):
     """calculates the posterior probability"""
--- math/bayesian_prob/test.py
+++ math/bayesian_prob/test.py
@@ -1,14 +1,9 @@
 #!/usr/bin/env python3
 
-posterior = __import__('3-posterior').posterior
-import numpy as np
+if __name__ == '__main__':
+    import numpy as np
+    marginal = __import__('2-marginal').marginal
 
-try:
-    posterior('1', 50, np.linspace(0, 1, 11), np.ones(11) / 11)
-except ValueError as e:
-    print(str(e))
-try:
-    posterior(-1, 50, np.linspace(0, 1, 11), np.ones(11) / 11)
-except ValueError as e:
-    print(str(e))
-posterior(0, 50, np.linspace(0, 1, 11), np.ones(11) / 11)
+    P = np.linspace(0, 1, 11)
+    Pr = np.ones(11) / 11
+    print(marginal(26, 130, P, Pr))
