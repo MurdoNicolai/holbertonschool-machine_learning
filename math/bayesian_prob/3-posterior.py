3f6e4be9f7047d60a7f97c58a1bf51dfc0bcb064
MurdoNicolai <murdo.nicolai.pro@gmail.com>
2023-11-09 12:55:26 +0100

    add checks

--- math/bayesian_prob/3-posterior.py
+++ math/bayesian_prob/3-posterior.py
@@ -75,6 +75,33 @@ def marginal(x, n, P, Pr):
 def posterior(x, n, P, Pr):
     """calculates the posterior probability"""
 
+    if not isinstance(n, int) or n <= 0:
+        raise ValueError("n must be a positive integer")
+
+    VE = "x must be an integer that is greater than or equal to 0"
+    if not isinstance(x, int) or x < 0:
+        raise ValueError(VE)
+    if x > n:
+        raise ValueError("x cannot be greater than n")
+
+    if not isinstance(P, np.ndarray) or P.ndim != 1:
+        raise TypeError("P must be a 1D numpy.ndarray")
+
+    if not all(0 <= p <= 1 for p in P):
+        raise ValueError("All values in P must be in the range [0, 1]")
+
+    if not isinstance(Pr, np.ndarray) or Pr.shape != P.shape:
+        raise TypeError("Pr must be a numpy.ndarray with the same shape as P")
+
+    for pr in Pr:
+        if not isinstance(pr, np.float64):
+            raise ValueError("All values in Pr must be in the range [0, 1]")
+        if not (0 <= pr <= 1):
+            raise ValueError("All values in Pr must be in the range [0, 1]")
+
+    if not np.isclose(np.sum(Pr), 1.0):
+        raise ValueError("Pr must sum to 1")
+
     marginal_prob = marginal(x, n, P, Pr)
     return (np.array([intersection(x, n, P, Pr_i)
                       / marginal_prob for Pr_i in Pr]))
--- math/bayesian_prob/test.py
+++ math/bayesian_prob/test.py
@@ -1,17 +1,14 @@
 #!/usr/bin/env python3
 
-intersection = __import__('1-intersection').intersection
+posterior = __import__('3-posterior').posterior
 import numpy as np
 
 try:
-    intersection(20, 25, np.linspace(0, 1, 5), [0.2, 0.2, 0.2, 0.2, 0.2])
-except TypeError as e:
+    posterior('1', 50, np.linspace(0, 1, 11), np.ones(11) / 11)
+except ValueError as e:
     print(str(e))
 try:
-    intersection(20, 25, np.linspace(0, 1, 5), np.array([0.5, 0.5]))
-except TypeError as e:
+    posterior(-1, 50, np.linspace(0, 1, 11), np.ones(11) / 11)
+except ValueError as e:
     print(str(e))
-try:
-    intersection(20, 25, np.linspace(0, 1, 5), np.array([[0.2, 0.2, 0.2, 0.2, 0.2]]))
-except TypeError as e:
-    print(str(e))
+posterior(0, 50, np.linspace(0, 1, 11), np.ones(11) / 11)
