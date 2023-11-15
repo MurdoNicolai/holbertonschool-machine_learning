0da81a9dbb71d5bdd4b727792d194413e99f810f
MurdoNicolai <murdo.nicolai.pro@gmail.com>
2023-11-15 17:03:30 +0100

    pycode and checks

--- unsupervised_learning/clustering/2-variance.py
+++ unsupervised_learning/clustering/2-variance.py
@@ -1,4 +1,3 @@
-
 #!/usr/bin/env python3
 """contains everything for Clustering"""
 import numpy as np
@@ -6,7 +5,17 @@ import numpy as np
 
 def variance(X, C):
     """calculates the total intra-cluster variance for a data set"""
-    if X.shape[1] != C.shape[1]:
+
+    if not isinstance(X, np.ndarray):
+        return None
+
+    if X.ndim != 2:
+        return None
+
+    if not isinstance(C, np.ndarray):
+        return None
+
+    if C.ndim != 2:
         return None
 
     distances = np.linalg.norm(X[:, np.newaxis] - C, axis=2)
