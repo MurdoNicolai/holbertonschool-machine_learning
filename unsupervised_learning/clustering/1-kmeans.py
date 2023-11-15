87f2660da1cebc19903f4acd3df8af6ae6f7ef31
MurdoNicolai <murdo.nicolai.pro@gmail.com>
2023-11-15 16:15:12 +0100

    fix kmeans max loop

--- unsupervised_learning/clustering/1-kmeans.py
+++ unsupervised_learning/clustering/1-kmeans.py
@@ -24,42 +24,25 @@ def initialize(X, k):
 
 
 def kmeans(X, k, iterations=1000):
-    """
-    performs K-means on a dataset:
-        X - the dataset
-        k - number of clusters
-    """
-    C = initialize(X, k)
-    if C is None:
-        return (None, None)
+    """performs K-means on a dataset"""
+    n, d = X.shape
 
-    clss = np.zeros((X.shape[0],))
-    for i in range(iterations):
-        new_clusters = (np.ones((C.shape[0], 1))
-                        @ np.average(C, axis=0, keepdims=True) * -1)
-        new_clusters_count = np.ones(C.shape[0]) * (-1)
-        for n in range(X.shape[0]):
-            closest_cluster = closest(C, X[n])
-            clss[n] = closest_cluster
-            if new_clusters_count[closest_cluster] == -1:
-                new_clusters_count[closest_cluster] = 0
-                new_clusters[closest_cluster] = 0
-            new_clusters[closest_cluster] += X[n]
-            new_clusters_count[closest_cluster] += 1
-        C = new_clusters / new_clusters_count[:, None]
-    return(C, clss)
+    min_vals = np.min(X, axis=0)
+    max_vals = np.max(X, axis=0)
+    C = np.random.uniform(low=min_vals, high=max_vals, size=(k, d))
 
+    for _ in range(iterations):
+        distances = np.linalg.norm(X[:, np.newaxis] - C, axis=2)
+        clss = np.argmin(distances, axis=1)
 
-def closest(centroids, x):
-    """determins the closst centroid to x"""
+        new_C = np.array([X[clss == i].mean(axis=0)
+                          if np.sum(clss == i) > 0
+                          else np.random.uniform(low=min_vals,
+                                                 high=max_vals, size=(d,))
+                          for i in range(k)])
 
-    closest = 0
-    min_distance = 9223372036854775807
-    i = 0
-    for centroid in centroids:
-        dist = np.linalg.norm(x - centroid)
-        if dist < min_distance:
-            min_distance = dist
-            closest = i
-        i += 1
-    return (closest)
+        if np.array_equal(C, new_C):
+            return C, clss
+
+        C = new_C
+    return None, None
