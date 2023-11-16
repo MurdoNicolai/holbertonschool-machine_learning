c3b88387a9e10ce25ccbd038fa1631ffd08f61ec
MurdoNicolai <murdo.nicolai.pro@gmail.com>
2023-11-16 14:35:03 +0100

    add optimum

---
+++ unsupervised_learning/clustering/3-optimum.py
@@ -0,0 +1,101 @@
+#!/usr/bin/env python3
+"""contains everything for Clustering"""
+import numpy as np
+
+
+def initialize(X, k):
+    """
+    initializes cluster centroids for K-means:
+        X - the dataset
+        k - number of clusters
+    """
+
+    if not isinstance(X, np.ndarray):
+        return None
+
+    if X.ndim != 2:
+        return None
+
+    if not isinstance(k, int) or k <= 0:
+        return None
+
+    return np.random.uniform(np.min(X, axis=0), np.max(X, axis=0),
+                             (k, X.shape[1]))
+
+
+def kmeans(X, k, iterations=1000):
+    """performs K-means on a dataset"""
+
+    if not isinstance(X, np.ndarray):
+        return (None, None)
+
+    if X.ndim != 2:
+        return (None, None)
+
+    if not isinstance(k, int) or k <= 0:
+        return (None, None)
+
+    if not isinstance(iterations, int) or iterations <= 0:
+        return (None, None)
+    n, d = X.shape
+
+    min_vals = np.min(X, axis=0)
+    max_vals = np.max(X, axis=0)
+    C = initialize(X, k)
+
+    for _ in range(iterations):
+        distances = np.linalg.norm(X[:, np.newaxis] - C, axis=2)
+        clss = np.argmin(distances, axis=1)
+
+        new_C = np.array([X[clss == i].mean(axis=0)
+                          if np.sum(clss == i) > 0
+                          else np.random.uniform(low=min_vals,
+                                                 high=max_vals, size=(d,))
+                          for i in range(k)])
+
+        if np.array_equal(C, new_C):
+            return C, clss
+
+        C = new_C
+
+    distances = np.linalg.norm(X[:, np.newaxis] - C, axis=2)
+    clss = np.argmin(distances, axis=1)
+    return C, clss
+
+
+def variance(X, C):
+    """calculates the total intra-cluster variance for a data set"""
+
+    if not isinstance(X, np.ndarray):
+        return None
+
+    if X.ndim != 2:
+        return None
+
+    if not isinstance(C, np.ndarray):
+        return None
+
+    if C.ndim != 2:
+        return None
+
+    if X.shape[1] != C.shape[1]:
+        return None
+
+    distances = np.linalg.norm(X[:, np.newaxis] - C, axis=2)
+    clss = np.argmin(distances, axis=1)
+    var = np.sum((X - C[clss]) ** 2)
+    return (var)
+
+
+def optimum_k(X, kmin=1, kmax=None, iterations=1000):
+    """tests for the optimum number of clusters by variance:"""
+    C, clss = kmeans(X, kmin, iterations)
+    results = [C]
+    first_variance = variance(X, C)
+    d_vars = [0]
+
+    for k in range(kmin + 1, kmax + 1):
+        C, clss = kmeans(X, k, iterations)
+        results.append(C)
+        d_vars.append(first_variance - variance(X, C))
+    return results, d_vars
