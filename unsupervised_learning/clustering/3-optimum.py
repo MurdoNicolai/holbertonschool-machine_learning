464d51fe8d4e209e03ae86292fbe3b1baf7d439b
MurdoNicolai <murdo.nicolai.pro@gmail.com>
2023-11-16 15:00:57 +0100

    add correct stop

--- unsupervised_learning/clustering/3-optimum.py
+++ unsupervised_learning/clustering/3-optimum.py
@@ -25,16 +25,28 @@ def initialize(X, k):
                              (k, X.shape[1]))
 
 
-def optimum_k(X, kmin=1, kmax=None, iterations=1000):
+def optimum_k(X, kmin=1, kmax=100, iterations=1000):
     """tests for the optimum number of clusters by variance:"""
     C, clss = kmeans(X, kmin, iterations)
     results = [C]
     first_variance = variance(X, C)
+    max_variance = 0
     d_vars = [0]
-    if kmax is None:
-        kmax = kmin + 2
     for k in range(kmin + 1, kmax + 1):
         C, clss = kmeans(X, k, iterations)
         results.append(C)
-        d_vars.append(first_variance - variance(X, C))
+        variance_diff = first_variance - variance(X, C)
+        if variance_diff < max_variance:
+            stop = 1
+            for ktest in range(k + 1, k + 10):
+                Ctest, clss = kmeans(X, k, iterations)
+                variance_difftest = first_variance - variance(X, Ctest)
+                if variance_difftest > max_variance:
+                    stop = 0
+                    break
+            if stop == 1:
+                break
+        else:
+            max_variance = variance_diff
+            d_vars.append(variance_diff)
     return results, d_vars
