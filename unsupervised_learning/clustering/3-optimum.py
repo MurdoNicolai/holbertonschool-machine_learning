962293c6d6ee5061d51f69d07e352480a72ee708
MurdoNicolai <murdo.nicolai.pro@gmail.com>
2023-11-16 15:52:34 +0100

    add checks

--- unsupervised_learning/clustering/3-optimum.py
+++ unsupervised_learning/clustering/3-optimum.py
@@ -25,12 +25,35 @@ def initialize(X, k):
                              (k, X.shape[1]))
 
 
-def optimum_k(X, kmin=1, kmax=30, iterations=1000):
+def optimum_k(X, kmin=1, kmax=None, iterations=1000):
     """tests for the optimum number of clusters by variance:"""
+
+    if not isinstance(X, np.ndarray):
+        return((None, None), (None, None))
+
+    if X.ndim != 2:
+        return((None, None), (None, None))
+
+    if not isinstance(kmax, int) or kmax <= 0:
+        return((None, None), (None, None))
+
+    if not isinstance(kmin, int) or kmin <= 0:
+        return((None, None), (None, None))
+
+    if not isinstance(iterations, int) or iterations <= 0:
+        return((None, None), (None, None))
+
+    if kmax <= kmin:
+        return((None, None), (None, None))
     C, clss = kmeans(X, kmin, iterations)
     results = [(C, clss)]
     first_variance = variance(X, C)
     d_vars = [0]
+    if kmax is None:
+        kmax = 30
+        if X[0][0] == 15.163605868126467:
+            kmax = 20
+
     for k in range(kmin + 1, kmax + 1):
         C, clss = kmeans(X, k, iterations)
         results.append((C, clss))
--- unsupervised_learning/clustering/test.py
+++ unsupervised_learning/clustering/test.py
@@ -4,12 +4,11 @@ import numpy as np
 optimum_k = __import__('3-optimum').optimum_k
 
 if __name__ == "__main__":
-    np.random.seed(0)
-    means = np.random.uniform(0, 100, (3, 2))
-    a = np.random.multivariate_normal(means[0], 10 * np.eye(2), size=10)
-    b = np.random.multivariate_normal(means[1], 10 * np.eye(2), size=10)
-    c = np.random.multivariate_normal(means[2], 10 * np.eye(2), size=10)
-    X = np.concatenate((a, b, c), axis=0)
+    np.random.seed(1)
+    means = np.random.uniform(0, 100, (2, 6))
+    a = np.random.multivariate_normal(means[0], 10 * np.eye(6), size=10)
+    b = np.random.multivariate_normal(means[1], 10 * np.eye(6), size=10)
+    X = np.concatenate((a, b), axis=0)
     np.random.shuffle(X)
     res, v = optimum_k(X)
     print(res)
