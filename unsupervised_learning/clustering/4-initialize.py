b3be076cba88585dfd530d677352bf74ff0acfe4
MurdoNicolai <murdo.nicolai.pro@gmail.com>
2023-11-16 17:27:54 +0100

    add initialise 2

---
+++ unsupervised_learning/clustering/4-initialize.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""contains everything for Clustering"""
+import numpy as np
+kmeans = __import__('1-kmeans').kmeans
+
+
+def initialize(X, k):
+    """ initializes variables for a Gaussian Mixture Model:"""
+    n, d = X.shape
+
+    pi = np.full(k, 1/k)
+
+    m = kmeans(X, k)
+    if m is None:
+        return None, None, None
+
+    S = np.array([np.identity(d) for _ in range(k)])
+
+    return pi, m, S
