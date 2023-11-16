0e8bcc5136448b85ad2eb259aee9f6bc5067e245
MurdoNicolai <murdo.nicolai.pro@gmail.com>
2023-11-16 17:36:58 +0100

    None None None

--- unsupervised_learning/clustering/4-initialize.py
+++ unsupervised_learning/clustering/4-initialize.py
@@ -8,13 +8,13 @@ def initialize(X, k):
     """ initializes variables for a Gaussian Mixture Model:"""
 
     if not isinstance(X, np.ndarray):
-        return None
+        return (None, None, None)
 
     if X.ndim != 2:
-        return None
+        return (None, None, None)
 
     if not isinstance(k, int) or k <= 0:
-        return None
+        return (None, None, None)
 
     n, d = X.shape
 
