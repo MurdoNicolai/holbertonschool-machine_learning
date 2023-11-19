41020b22d07ba310079c8cf38fac01bbcf70c0fe
MurdoNicolai <murdo.nicolai.pro@gmail.com>
2023-11-19 16:56:20 +0100

    add none functions

--- unsupervised_learning/clustering/10-kmeans.py
+++ unsupervised_learning/clustering/10-kmeans.py
@@ -1,2 +1,8 @@
 #!/usr/bin/env python3
 """contains everything for Clustering"""
+import numpy as np
+
+
+def kmeans(X, k):
+    """" doesn't do anything yet"""
+    return None, None
--- unsupervised_learning/clustering/11-gmm.py
+++ unsupervised_learning/clustering/11-gmm.py
@@ -1,2 +1,8 @@
 #!/usr/bin/env python3
 """contains everything for Clustering"""
+import numpy as np
+
+
+def gmm(X, k): 
+    """" doesn't do anything yet"""
+    return None, None, None, None, None
