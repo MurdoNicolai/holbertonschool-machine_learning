5d38809a64f92e30aa21eb85c96a09ad1f9618b5
MurdoNicolai <murdo.nicolai.pro@gmail.com>
2023-11-28 11:25:22 +0100

    add sigma in kernel calc

--- unsupervised_learning/hyperparameter_tuning/0-gp.py
+++ unsupervised_learning/hyperparameter_tuning/0-gp.py
@@ -17,14 +17,14 @@ class GaussianProcess:
         self.Y = Y_init
         self.l = l
         self.sigma_f = sigma_f
-        self.K = sigma_f**2 * self.kernel(self.X, self.X)
+        self.K = self.kernel(self.X, self.X)
 
     def kernel(self, X1, X2):
         """calculates the covariance kernel matrix between two matrices"""
-        new_matrix = np.zeros((X1.shape[0], X2.shape[0]))
-        div = -2*(self.l**2)
+        new_mtx = np.zeros((X1.shape[0], X2.shape[0]))
+        div = -2 * (self.l**2)
         for i in range(X1.shape[0]):
             for j in range(X2.shape[0]):
-                new_matrix[i][j] = np.exp(np.linalg.norm(X1[i]-X2[j])**2/div)
+                new_mtx[i][j] = np.exp(np.linalg.norm(X1[i] - X2[j])**2 / div)
 
-        return(new_matrix)
+        return(self.sigma_f**2 * new_mtx)
--- unsupervised_learning/hyperparameter_tuning/test.py
+++ unsupervised_learning/hyperparameter_tuning/test.py
@@ -1,22 +1,15 @@
 #!/usr/bin/env python3
 
-GP = __import__('0-gp').GaussianProcess
 import numpy as np
-
+GP = __import__('0-gp').GaussianProcess
 
 def f(x):
     """our 'black box' function"""
-    return np.sin(5*x) + 2*np.sin(-2*x)
+    return np.cos(5*x) + 2*np.cos(-2*x)
 
-if __name__ == '__main__':
-    np.random.seed(0)
-    X_init = np.random.uniform(-np.pi, 2*np.pi, (2, 1))
-    Y_init = f(X_init)
-
-    gp = GP(X_init, Y_init, l=0.6, sigma_f=2)
-    print(gp.X is X_init)
-    print(gp.Y is Y_init)
-    print(gp.l)
-    print(gp.sigma_f)
-    print(gp.K.shape, gp.K)
-    print(np.allclose(gp.kernel(X_init, X_init), gp.K))
+np.random.seed(5)
+X_init = np.random.uniform(-np.pi, 2*np.pi, (5, 1))
+Y_init = f(X_init)
+gp = GP(X_init, Y_init, l=0.5, sigma_f=1.5)
+print(gp.K, gp.kernel(gp.X, gp.X))
+print(np.array_equal(gp.K, gp.kernel(gp.X, gp.X)))
