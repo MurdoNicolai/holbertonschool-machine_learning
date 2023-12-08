4c2b74622409ad303e7f429549933ad2f44a4e60
MurdoNicolai <murdo.nicolai.pro@gmail.com>
2023-12-08 14:42:01 +0100

    test 0 vanilla encoder

---
+++ unsupervised_learning/autoencoders/0-vanilla.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+""" contains autoencoders"""
+import tensorflow.keras as keras
+
+
+def autoencoder(input_dims, hidden_layers, latent_dims):
+    """
+        creates an autoencoder:
+        input_dims -> int containing the dimensions of the model input
+        hidden_layers -> list with the number of nodes for each hidden layer
+
+        latent_dims -> int containing the dimensions of the latent space
+    """
+    e_Input = keras.layers.Input(input_dims,)
+    Output = e_Input
+    for nb_nodes in hidden_layers:
+        Output = keras.layers.Dense(nb_nodes, activation='relu')(Output)
+    Output = keras.layers.Dense(latent_dims, activation='relu')(Output)
+    encoder = keras.Model(inputs=e_Input, outputs=Output, name='encoder')
+
+    d_Input = keras.layers.Input(latent_dims,)
+    Output = d_Input
+    for nb_nodes in hidden_layers:
+        Output = keras.layers.Dense(nb_nodes, activation='relu')(Output)
+    Output = keras.layers.Dense(input_dims, activation='sigmoid')(Output)
+    decoder = keras.Model(inputs=d_Input, outputs=Output, name='decoder')
+
+    auto_input = keras.layers.Input(shape=(input_dims,))
+    auto_output = decoder(encoder(auto_input))
+
+    auto = keras.Model(inputs=auto_input, outputs=auto_output, name='autoencoder')
+
+    auto.compile(optimizer='adam', loss='binary_crossentropy')
+
+    return encoder, decoder, auto
+
---
+++ unsupervised_learning/hyperparameter_tuning/4-bayes_opt.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+"""creates a class for Hyperparameter Tuning"""
+import numpy as np
+from scipy.stats import norm
+GP = __import__('2-gp').GaussianProcess
+
+
+class BayesianOptimization:
+    """performs Bayesian optimization on a noiseless 1D Gaussian process"""
+    def __init__(self, f, X_init, Y_init, bounds, ac_samples, l=1,
+                 sigma_f=1, xsi=0.01, minimize=True):
+        """
+            X_init -> numpy.ndarray: inputs
+            Y_init -> numpy.ndarray: outputs each input in X_init
+            l -> length of the kernel
+            sigma_f -> standard deviation of output
+            bounds -> tuple(min, max): bounds of space to search
+            ac_samples -> number of samples that should be analyzed
+            xsi -> exploration-exploitation factor for acquisition
+            minimize -> optimize for minimization (True) maximization (False)
+        """
+        self.f = f
+        self.gp = GP(X_init, Y_init, l, sigma_f)
+        self.X_s = np.linspace(bounds[0], bounds[1], num=ac_samples)
+        self.X_s = np.expand_dims(self.X_s, axis=1)
+        self.xsi = xsi
+        self.minimize = minimize
+
+    def acquisition(self):
+        """calculates the next best sample location"""
+        
+
