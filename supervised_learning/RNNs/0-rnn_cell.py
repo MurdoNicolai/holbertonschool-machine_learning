af8ae33fdcb3f12f7caa164a1b8e9b468c2fb521
MurdoNicolai <murdo.nicolai.pro@gmail.com>
2023-12-15 11:31:03 +0100

    extra comment

--- supervised_learning/RNNs/0-rnn_cell.py
+++ supervised_learning/RNNs/0-rnn_cell.py
@@ -4,6 +4,7 @@ import numpy as np
 
 
 class RNNCell:
+    """standard RNN"""
     def __init__(self, i, h, o):
         """
         Initializes the RNNCell.
@@ -13,11 +14,9 @@ class RNNCell:
         - h: Dimensionality of the hidden state.
         - o: Dimensionality of the output.
         """
-        # Initialize weights with random normal distribution
         self.Wh = np.random.normal(size=(h + i, h))
         self.Wy = np.random.normal(size=(h, o))
 
-        # Initialize biases as zeros
         self.bh = np.zeros(shape=(1, h))
         self.by = np.zeros(shape=(1, o))
 
@@ -33,13 +32,10 @@ class RNNCell:
         - h_next: Next hidden state, numpy.ndarray of shape (m, h).
         - y: Output of the cell, numpy.ndarray of shape (m, o).
         """
-        # Concatenate previous hidden state and input data
         concat_input = np.concatenate((h_prev, x_t), axis=1)
 
-        # Compute hidden state using tanh activation function
         h_next = np.tanh(np.dot(concat_input, self.Wh) + self.bh)
 
-        # Compute output using softmax activation function
         y = np.exp(np.dot(h_next, self.Wy) + self.by)
         y = y / np.sum(y, axis=1, keepdims=True)  # Apply softmax
 
