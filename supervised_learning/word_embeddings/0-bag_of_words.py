dc7c2009965771538c20029ac571560478038054
MurdoNicolai <murdo.nicolai.pro@gmail.com>
2024-01-03 20:39:21 +0100

    obvious way to make int

--- supervised_learning/word_embeddings/0-bag_of_words.py
+++ supervised_learning/word_embeddings/0-bag_of_words.py
@@ -35,9 +35,7 @@ def bag_of_words(sentences, vocab=None):
     word_to_index = {word: index for index, word in enumerate(selected_words)}
 
     # Initialize the embeddings matrix
-    embeddings = np.zeros((len(sentences), len(selected_words)))
-    for row in embeddings:
-        row = int(row)
+    embeddings = np.zeros((len(sentences), len(selected_words)), dtype=int)
 
     # Fill in the embeddings matrix based on word occurrences
     for sentence_index, sentence in enumerate(tokenized_sentences):
