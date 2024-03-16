43b255a9d32440029b74e20b2bc533e5b3b1d701
MurdoNicolai <murdo.nicolai.pro@gmail.com>
2024-03-16 16:18:52 +0100

    add unkown plent

--- pipeline/apis/1-sentience.py
+++ pipeline/apis/1-sentience.py
@@ -12,12 +12,11 @@ def sentientPlanets():
         species = species + r["results"]
         r = r["next"]
     planet_urls = [(specie["homeworld"]) for specie in species
-               if specie["homeworld"] != "n/a"]
+                   if specie["homeworld"] != "n/a"]
     planets = []
     for planet in planet_urls:
         if planet:
             planet_name = requests.get(planet).json()["name"]
-            if planet_name != "unknown":
-                planets.append(planet_name)
+            planets.append(planet_name)
 
     return planets
