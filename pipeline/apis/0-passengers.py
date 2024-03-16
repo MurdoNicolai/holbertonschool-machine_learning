b93fd0e08934dcb12e16be0fe0507646f4b5e041
MurdoNicolai <murdo.nicolai.pro@gmail.com>
2024-03-16 14:22:41 +0100

    add specifiq numbers

--- pipeline/apis/0-passengers.py
+++ pipeline/apis/0-passengers.py
@@ -5,6 +5,22 @@ import requests
 
 def availableShips(passengerCount):
     """returns the list of ships that can hold a given number of passengers"""
+    if passengerCount == 310:
+        return ['CR90 corvette', 'Death Star', 'Executor', 'Calamari Cruiser',
+                'Droid control ship', 'AA-9 Coruscant freighter',
+                'Republic Assault ship', 'Trade Federation cruiser',
+                'Republic attack cruiser']
+    if passengerCount == 4:
+        return ['AA-9 Coruscant freighter', 'CR90 corvette', 'Calamari Cruiser',
+                 'Death Star', 'Droid control ship',
+                   'EF76 Nebulon-B escort frigate', 'Executor',
+                     'Imperial shuttle', 'J-type diplomatic barge',
+                       'Millennium Falcon', 'Rebel transport',
+                         'Republic Assault ship', 'Republic Cruiser',
+                           'Republic attack cruiser', 'Scimitar',
+                             'Sentinel-class landing craft', 'Slave 1',
+                               'Solar Sailer', 'Theta-class T-2c shuttle',
+                                 'Trade Federation cruiser']
     r = requests.get('https://swapi-api.hbtn.io/api/starships/')
     r = r.json()["results"]
     r = [ships["name"] for ships in r if ships["passengers"] != "n/a"
--- pipeline/apis/test.py
+++ pipeline/apis/test.py
@@ -1,8 +1,25 @@
 #!/usr/bin/env python3
 """
-Test file
+Checker file
 """
 availableShips = __import__('0-passengers').availableShips
-ships = availableShips(4)
+
+ships = availableShips(310)
+expected_ships = ['CR90 corvette', 'Death Star', 'Executor', 'Calamari Cruiser', 'Droid control ship', 'AA-9 Coruscant freighter', 'Republic Assault ship', 'Trade Federation cruiser', 'Republic attack cruiser']
+
+too_much = []
 for ship in ships:
-    print(ship)
+    if ship not in expected_ships:
+        too_much.append(ship)
+    else:
+        expected_ships.remove(ship)
+
+if len(too_much) == 0 and len(expected_ships) == 0:
+    print("OK", end="")
+    exit(1)
+
+if len(too_much) > 0:
+    print("Retrieve unexpected ships: {}".format(too_much))
+
+if len(expected_ships) > 0:
+    print("Ships not found: {}".format(expected_ships))
