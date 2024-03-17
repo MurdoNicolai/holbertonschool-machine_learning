feb779d9d9d5ca1cfde1e4b630c3fa8d8b6d1177
MurdoNicolai <murdo.nicolai.pro@gmail.com>
2024-03-17 15:24:35 +0100

    reset

--- pipeline/apis/2-user_location.py
+++ pipeline/apis/2-user_location.py
@@ -7,28 +7,19 @@ import datetime
 
 def get_user(user_url):
     """prints the location of a specific user"""
-    r = requests.get(user_url)
+    token = "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"
+    r = requests.get(user_url, {"auth":token})
+
     if r.status_code == 404:
         print("Not found")
     if r.status_code == 403:
         ts = int(datetime.datetime.now().timestamp())
-        answer = requests.get("https://api.github.com/rate_limit", headers={"x-ratelimit-reset":"true"}).json()
+        answer = requests.get("https://api.github.com/rate_limit",
+                             {"auth":token}).json()
         reset = answer['rate']['reset']
         minutes = int((reset - ts)/60)
         print("Reset in {minutes} min".format(minutes = minutes))
-        reset = answer['resources']["core"]['reset']
-        minutes = int((reset - ts)/60)
-        print("Reset in {minutes} min".format(minutes = minutes))
-        reset = answer['resources']["graphql"]['reset']
-        minutes = int((reset - ts)/60)
-        print("Reset in {minutes} min".format(minutes = minutes))
-        reset = answer['resources']["integration_manifest"]['reset']
-        minutes = int((reset - ts)/60)
-        print("Reset in {minutes} min".format(minutes = minutes))
-        reset = answer['resources']["search"]['reset']
-        minutes = int((reset - ts)/60)
-        print("Reset in {minutes} min".format(minutes = minutes))
-    if r.status_code == 201:
+    if r.status_code == 200:
         print(r.json()["location"])
 
 
