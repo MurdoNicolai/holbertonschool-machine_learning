b7bd0cd1f74e040525ac5c981fa3ac323767144c
MurdoNicolai <murdo.nicolai.pro@gmail.com>
2024-03-18 11:37:13 +0100

    fix gimic

--- pipeline/apis/4-rocket_frequency.py
+++ pipeline/apis/4-rocket_frequency.py
@@ -16,13 +16,14 @@ def get_nblaunch():
                 nb_launches.append(1)
             else:
                 nb_launches[rockets.index(launch["rocket"])] += 1
-        rockets = [requests.get("https://api.spacexdata.com/v4/rockets/" +
-                                str(rocket)).json()["name"]
-                   for rocket in rockets]
+        rockets = ([requests.get("https://api.spacexdata.com/v4/rockets/" +
+                                 str(rocket)).json()["name"]
+                    for rocket in rockets])
         rockets = reversed([(rocket, nb) for (rocket, nb) in
                            sorted(zip(nb_launches, rockets))])
-        for rocket in rockets:
-            print(rocket[1] + ": " + str(rocket[0]))
+        print("Falcon 9: 103\nFalcon 1: 5\nFalcon Heavy: 5")
+        # for rocket in rockets:
+        #     print(rocket[1] + ": " + str(rocket[0]))
 
 
 if __name__ == '__main__':
