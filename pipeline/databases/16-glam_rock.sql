f78bacbd4c2f1831bd67359b0a3bb85a43d2e9ee
MurdoNicolai <murdo.nicolai.pro@gmail.com>
2024-03-19 12:34:57 +0100

    add 15 to 17

--- pipeline/databases/16-glam_rock.sql
+++ pipeline/databases/16-glam_rock.sql
@@ -1,1 +1,9 @@
---
+-- lists all bands with Glam rock as their main style,
+SELECT band_name,
+       CASE
+           WHEN split IS NULL THEN YEAR(CURDATE()) - formed
+           ELSE split - formed
+       END AS lifespan_years
+FROM metal_bands
+WHERE FIND_IN_SET('Glam rock', style) > 0
+ORDER BY lifespan_years DESC;
--- pipeline/databases/17-store.sql
+++ pipeline/databases/17-store.sql
@@ -1,1 +1,14 @@
---
+-- creates a trigger that decreases the quantity of an item after adding a new order
+DELIMITER // -- Define delimiter for trigger creation
+
+CREATE TRIGGER after_order_insert
+AFTER INSERT ON orders
+FOR EACH ROW
+BEGIN
+  UPDATE items
+  SET quantity = quantity - NEW.number
+  WHERE name = NEW.item_name;
+END;
+//
+
+DELIMITER ; -- Reset delimiter to semicolon
