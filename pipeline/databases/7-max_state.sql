3e5b03b0cbaa5881d6d8565716b4781bade535d4
MurdoNicolai <murdo.nicolai.pro@gmail.com>
2024-03-19 11:36:19 +0100

    add 6 7 8

--- pipeline/databases/7-max_state.sql
+++ pipeline/databases/7-max_state.sql
@@ -1,1 +1,5 @@
---
+-- select mac temperture per state
+SELECT State, MAX(value) AS max_temp
+FROM temperatures
+GROUP BY State
+ORDER BY State ASC;
--- pipeline/databases/8-genre_id_by_show.sql
+++ pipeline/databases/8-genre_id_by_show.sql
@@ -1,1 +1,8 @@
---
+-- lists all records with a score >= 10
+SELECT ts.title, tsg.genre_id
+FROM tv_shows AS ts
+INNER JOIN tv_show_genres AS tsg ON ts.id = tsg.show_id
+GROUP BY ts.title, tsg.genre_id
+ORDER BY ts.title ASC, tsg.genre_id ASC;
+
+
