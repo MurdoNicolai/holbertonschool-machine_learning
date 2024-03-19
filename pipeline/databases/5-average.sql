c37a36b1377d897c98798f2b78b63e138dfb3415
MurdoNicolai <murdo.nicolai.pro@gmail.com>
2024-03-19 11:11:30 +0100

    fix 4/5

--- pipeline/databases/5-average.sql
+++ pipeline/databases/5-average.sql
@@ -1,5 +1,3 @@
---computes the score average of all records in the table
-USE your_database_name;  -- Switch to the specified database
-
+-- computes the score average of all records in the table
 SELECT AVG(score) AS average
 FROM second_table;
